8164c4b8142841150c53136dd7337d2838cc9301
swithun <2571021108@qq.com>
2023-03-04 13:03:46 +0800

    实现load base path

--- frontend/swithunApp/app/src/main/java/com/example/myapplication/viewmodel/FileManagerViewModel.kt
+++ frontend/swithunApp/app/src/main/java/com/example/myapplication/viewmodel/FileManagerViewModel.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.myapplication.ActivityVar
+import com.example.myapplication.SwithunLog
 import com.example.myapplication.model.KernelAndFrontEndJson
 import com.example.myapplication.nullCheck
 import com.example.myapplication.util.UrlEncodeParams
@@ -13,20 +14,20 @@ import com.example.myapplication.util.getRequest
 import com.example.myapplication.util.getRequestWithOriginalResponse
 import com.example.myapplication.websocket.RawData
 import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.reflect.TypeToken
 import com.koushikdutta.async.http.HttpUtil
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.File
+import java.lang.Exception
 import java.util.*
 
-class FileManagerViewModel: ViewModel() {
+class FileManagerViewModel : ViewModel() {
 
     val fileBasePath: String = Environment.getExternalStorageDirectory().absolutePath
     var pathList: List<PathItem> by mutableStateOf(listOf())
     var activityVar: ActivityVar? = null
-
-    init {
-        val basePath = File(fileBasePath)
-        pathList = basePath.listFiles()?.map2Items() ?: emptyList()
-    }
 
     fun init(activityVar: ActivityVar) {
         this.activityVar = activityVar
@@ -42,7 +43,8 @@ class FileManagerViewModel: ViewModel() {
             else -> {
                 folder.isOpening = true
                 val parent = File(folder.path)
-                folder.children = parent.listFiles()?.map2Items().nullCheck("children: ") ?: emptyList()
+                folder.children =
+                    parent.listFiles()?.map2Items().nullCheck("children: ") ?: emptyList()
             }
         }
         // todo 看看怎么做不用这样赋值
@@ -80,7 +82,7 @@ class FileManagerViewModel: ViewModel() {
         return basePath.listFiles()?.map2LocalItems() ?: emptyList()
     }
 
-    fun getBasePathList(){
+    fun getBasePathList() {
         activityVar?.wordsVM?.let {
             val uuid = UUID.randomUUID().toString()
             val j = KernelAndFrontEndJson(
@@ -99,21 +101,58 @@ class FileManagerViewModel: ViewModel() {
         val urlEncodeParams = UrlEncodeParams().apply {
             put("path", "base")
         }
-        getRequestWithOriginalResponse("http://192.168.0.109:8088/get_path_list", urlEncodeParams)?.body?.string().nullCheck("response: ", true)
+        val pathListJsonStr: String = getRequestWithOriginalResponse(
+            "http://192.168.0.109:8088/get_path_list",
+            urlEncodeParams
+        )?.body?.string().nullCheck("response: ", true)
+            ?: return
+        try {
+            val pathItemArrayList = JSONArray(pathListJsonStr)
+            val pathItemList = mutableListOf<PathItem>()
+            for (i in 0 until pathItemArrayList.length()) {
+                val it = pathItemArrayList.get(i) as? JSONObject ?: continue
+                val fileTypeInt = it.getInt("fileType")
+                val fileType =
+                    LocalPathItem.PathType.fromValue(fileTypeInt).nullCheck("解析的fileType") ?: return
+                val path = it.getString("path")
+                val item = when (fileType) {
+                    LocalPathItem.PathType.FILE -> {
+                        PathItem.FileItem(path)
+                    }
+                    LocalPathItem.PathType.Folder -> {
+                        PathItem.FolderItem(path, emptyList())
+                    }
+                }
+                pathItemList.add(item)
+            }
+            pathList = pathItemList
+
+        } catch (e: Exception) {
+            SwithunLog.e("http path 解析失败")
+        }
     }
 }
 
 sealed class PathItem(val path: String) {
     val name = path
+
     class FolderItem(path: String, var children: List<PathItem>) : PathItem(path) {
         var isOpening = false
     }
+
     class FileItem(path: String) : PathItem(path)
 }
 
 data class LocalPathItem(val path: String, val fileType: Int) {
+
     enum class PathType(val type: Int) {
         FILE(0),
-        Folder(1)
+        Folder(1);
+
+        companion object {
+            fun fromValue(value: Int): PathType? = values().find { it.type == value }
+        }
+
     }
+
 }
